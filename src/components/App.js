3be9c9abc6e4d5538012c15412db11f401607956
kristoferdowe <kristoferdowe@gmail.com>
2020-12-02 18:43:07 -0500

    styling broken

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,12 @@
 import React from "react";
 import Userlist from "./userlists";
 import Login from "./login";
+import {
+  
+  Stylesheet,
+  ImageBackground
+}
+from "react-native";
 
 //https://stackoverflow.com/questions/4999064/regex-for-string-contains - resource used for parsing method
 
@@ -14,129 +20,129 @@ class App extends React.Component {
 
     this.handleSubmit = this.handleSubmit.bind(this);
 
-    //this.handleChange = this.handleChange.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  //ALL PARSING LOGIC - Needs to go to backend
+  //ALL PARSING LOGIC 
 
-  // handleChange(event) {
-  //   const parseDayZProfile = async e => {
-  //     // asynchronous event listener for input change.
-  //     // get the uploaded file and...
-  //     const f = e.target.files[0];
+   handleChange(event) {
+     const parseDayZProfile = async e => {
+       // asynchronous event listener for input change.
+       // get the uploaded file and...
+       const f = e.target.files[0];
 
-  //     //...parse it to some text
-  //     let t = await f.text();
+      //...parse it to some text
+       let t = await f.text();
 
-  //     // set divider and make sure reserved words are used correctly
-  //     const divider = "Survivor";
-  //     const reserved = [divider];
+       // set divider and make sure reserved words are used correctly
+       const divider = "Survivor";
+       const reserved = [divider];
 
-  //     // body parts that the gear is equipped on.
+       // body parts that the gear is equipped on.
 
-  //     const parts = [
-  //       "Body",
-  //       "Legs",
-  //       "Feet",
-  //       "Arms",
-  //       "Hands",
-  //       "Head",
-  //       "Chest",
-  //       "Back"
-  //     ];
+       const parts = [
+         "Body",
+         "Legs",
+         "Feet",
+         "Arms",
+         "Hands",
+         "Head",
+        "Chest",
+         "Back"
+       ];
 
-  //     // regular expression that matches character names in data
-  //     const nameRegex = /Survivor[M | F]_[A-Za-z]+/gim;
+       // regular expression that matches character names in data
+       const nameRegex = /Survivor[M | F]_[A-Za-z]+/gim;
 
-  //     // structure to hold character equip data
-  //     const structure = {};
+       // structure to hold character equip data
+       const structure = {};
 
-  //     // remove control characters, excess spaces, items that are not valid words,
-  //     // items that are messed up in some way
-  //     //may have to add to the list later as new items are encountered and logged for the first time
-  //     t = t
-  //       .replace(/[\x00-\x1F\x7F-\x9F\W]/gm, " ")
-  //       .replace(/\s+/gim, " ")
-  //       .trim()
-  //       .split(" ")
-  //       .filter(item => item.length > 3)
-  //       .filter(item => !/^[a-z]/gm.test(item[0]))
-  //       .filter(item => {
-  //         const b = item
-  //           .split("")
-  //           .sort()
-  //           .join("")
-  //           .replace(/\d/gm, "");
+       // remove control characters, excess spaces, items that are not valid words,
+       // items that are messed up in some way
+       
+       t = t
+         .replace(/[\x00-\x1F\x7F-\x9F\W]/gm, " ")
+         .replace(/\s+/gim, " ")
+         .trim()
+         .split(" ")
+         .filter(item => item.length > 3)
+         .filter(item => !/^[a-z]/gm.test(item[0]))
+         .filter(item => {
+           const b = item
+             .split("")
+             .sort()
+             .join("")
+             .replace(/\d/gm, "");
 
-  //         return !/[A-Z]{4,}/gm.test(b);
-  //       });
+           return !/[A-Z]{4,}/gm.test(b);
+         });
 
-  //     // add body parts to character objects
-  //     t.forEach(item => {
-  //       if (item === divider) {
-  //         return;
-  //       }
+       // add body parts to character objects
+       t.forEach(item => {
+         if (item === divider) {
+           return;
+         }
 
-  //       const surv = structure;
+         const surv = structure;
 
-  //       // if current item is a character, then add on all the body part arrays
-  //       if (nameRegex.test(item)) {
-  //         surv[item] = {};
-  //         parts.forEach(p => (surv[item][p] = []));
-  //         reserved.push(item);
-  //       }
-  //     });
+         // if current item is a character, then add on all the body part arrays
+         if (nameRegex.test(item)) {
+           surv[item] = {};
+          parts.forEach(p => (surv[item][p] = []));
+           reserved.push(item);
+         }
+       });
 
-  //     // filter out extraneous names in data
-  //     t = t.filter(n => !nameRegex.test(n));
+      // filter out extraneous names in data
+       t = t.filter(n => !nameRegex.test(n));
 
-  //     // set up temp array for equipment
-  //     let temp = [];
+       // set up temp array for equipment
+      let temp = [];
 
-  //     // initialize flags to hold the current operating character, and current body part
-  //     let currentPerson, currentPart;
+       // initialize flags to hold the current operating character, and current body part
+       let currentPerson, currentPart;
 
-  //     // for each item in the array of text,
-  //     t.forEach(item => {
-  //       const s = structure;
+       // for each item in the array of text,
+       t.forEach(item => {
+         const s = structure;
 
-  //       // if the item is not a body part or a reserved keyword,
-  //       if (!parts.includes(item) && !reserved.includes(item)) {
-  //         // add the item into the array. it should be an equipment piece
-  //         temp.push(item);
-  //       } else {
-  //         // otherwise, set the current body part.
-  //         if (parts.includes(item)) {
-  //           currentPart = item;
+        // if the item is not a body part or a reserved keyword,
+        if (!parts.includes(item) && !reserved.includes(item)) {
+           // add the item into the array. it should be an equipment piece
+           temp.push(item);
+         } else {
+           // otherwise, set the current body part.
+           if (parts.includes(item)) {
+             currentPart = item;
 
-  //           // go through the temp array, which is now full of equipment,
-  //           temp.forEach(t => {
-  //             // and add on equipment to the current body part on the current person.
-  //             s[currentPerson][currentPart].push(t);
-  //           });
+             // go through the temp array, which is now full of equipment,
+             temp.forEach(t => {
+               // and add on equipment to the current body part on the current person.
+               s[currentPerson][currentPart].push(t);
+             });
 
-  //           // empty out the temporary 'gear holder'
-  //           temp = [];
-  //         } else if (reserved.includes(item) && item !== divider) {
-  //           // if a new name is found, set the current person to the new name
+             // empty out the temporary 'gear holder'
+             temp = [];
+           } else if (reserved.includes(item) && item !== divider) {
+             // if a new name is found, set the current person to the new name
 
-  //           currentPerson = item;
-  //         }
-  //       }
-  //     });
+             currentPerson = item;
+           }
+         }
+       });
 
-  //     // return the created object
-  //     return structure;
-  //   };
-  //   parseDayZProfile(event).then(survivorList =>
-  //     this.setState({
-  //       inventory: {
-  //         survivorList
-  //       }
-  //     })
-  //   );
-  //   this.setState({ value: event.target.value });
-  // }
+       // return the created object
+       return structure;
+     };
+     parseDayZProfile(event).then(survivorList =>
+       this.setState({
+         inventory: {
+           survivorList
+         }
+       })
+     );
+     this.setState({ value: event.target.value });
+   }
   onChangeLoadout() {
     console.log("loadout submitted");
     //   //this.state.inventory;
@@ -152,9 +158,13 @@ class App extends React.Component {
   }
 
   render() {
+
     console.log(this.state.inventory.survivorList);
     return (
-      <div className="container-2">
+      <ImageBackground source = {require('./app/img/bg.JPG')}
+                        style={styles.container}>
+      <div className="container-2" >
+        <Login />
         <form onSubmit={this.handleSubmit}>
           <label>
             <input
@@ -164,14 +174,36 @@ class App extends React.Component {
             />
           </label>
         </form>
-        <button onClick={this.onChangeLoadout.bind(this)} className="btn">
-          Post
+        
+        
+        <button onClick={this.onChangeLoadout.bind(this)} style={{
+          font: 'inherit',
+          cursor: 'pointer',
+          border: '1px solid blue',
+          background: 'blue',
+          color: 'white',
+          padding: '1rem 2rem'
+        }}>
+        Post
         </button>
-        <Login />
+        
         <Userlist survivorList={this.state.inventory.survivorList} />
       </div>
-    );
-  }
-}
+    
+  
+
+</ImageBackground>
+  );
+      }}
+
+       const styles = Stylesheet.create({
+         container: {
+           flex: 1,
+           alignItems: 'center',
+           justifyContent: 'center'
+        
+
+         },
+       });
 
 export default App;
